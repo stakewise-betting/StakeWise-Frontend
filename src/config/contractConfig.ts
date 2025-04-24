41e7aa2d1291d4fbb92c159795f4ae6a98e12dc7
Hiruna Warusawithana <hirunawarusawithana@Hirunas-MacBook-Pro.local>
2025-04-24 17:32:02 +0530

    fixxing errors

--- src/config/contractConfig.ts
+++ src/config/contractConfig.ts
@@ -1,479 +1,412 @@
 // config/contractConfig.ts
 
-
-export const contractAddress = "0x90cf8A74ED31D80eC41A33fD6E2ee7e6bE4E7E3A";
-
+export const contractAddress = "0xeaaE4D789c4f82B8e36c1e294114F97833603992";
 
 export const contractABI = [
   {
-    "inputs": [],
-    "stateMutability": "nonpayable",
-    "type": "constructor"
+    inputs: [],
+    stateMutability: "nonpayable",
+    type: "constructor",
   },
   {
-    "anonymous": false,
-    "inputs": [
+    anonymous: false,
+    inputs: [
       {
-        "indexed": false,
-        "internalType": "uint256",
-        "name": "eventId",
-        "type": "uint256"
+        indexed: false,
+        internalType: "uint256",
+        name: "eventId",
+        type: "uint256",
       },
       {
-        "indexed": false,
-        "internalType": "address",
-        "name": "bettor",
-        "type": "address"
+        indexed: false,
+        internalType: "address",
+        name: "bettor",
+        type: "address",
       },
       {
-        "indexed": false,
-        "internalType": "uint256",
-        "name": "amount",
-        "type": "uint256"
+        indexed: false,
+        internalType: "uint256",
+        name: "amount",
+        type: "uint256",
       },
       {
-        "indexed": false,
-        "internalType": "string",
-        "name": "option",
-        "type": "string"
-      }
+        indexed: false,
+        internalType: "string",
+        name: "option",
+        type: "string",
+      },
     ],
-    "name": "BetPlaced",
-    "type": "event"
+    name: "BetPlaced",
+    type: "event",
   },
   {
-    "anonymous": false,
-    "inputs": [
+    anonymous: false,
+    inputs: [
       {
-        "indexed": false,
-        "internalType": "uint256",
-        "name": "eventId",
-        "type": "uint256"
+        indexed: false,
+        internalType: "uint256",
+        name: "eventId",
+        type: "uint256",
       },
       {
-        "indexed": false,
-        "internalType": "string",
-        "name": "name",
-        "type": "string"
+        indexed: false,
+        internalType: "string",
+        name: "name",
+        type: "string",
       },
       {
-        "indexed": false,
-        "internalType": "uint256",
-        "name": "startTime",
-        "type": "uint256"
+        indexed: false,
+        internalType: "uint256",
+        name: "startTime",
+        type: "uint256",
       },
       {
-        "indexed": false,
-        "internalType": "uint256",
-        "name": "endTime",
-        "type": "uint256"
-      }
+        indexed: false,
+        internalType: "uint256",
+        name: "endTime",
+        type: "uint256",
+      },
     ],
-    "name": "EventCreated",
-    "type": "event"
+    name: "EventCreated",
+    type: "event",
   },
   {
-    "anonymous": false,
-    "inputs": [
+    anonymous: false,
+    inputs: [
       {
-        "indexed": false,
-        "internalType": "uint256",
-        "name": "eventId",
-        "type": "uint256"
+        indexed: false,
+        internalType: "uint256",
+        name: "eventId",
+        type: "uint256",
       },
       {
-        "indexed": false,
-        "internalType": "string",
-        "name": "winningOption",
-        "type": "string"
-      }
+        indexed: false,
+        internalType: "string",
+        name: "winningOption",
+        type: "string",
+      },
     ],
-    "name": "WinnerDeclared",
-    "type": "event"
+    name: "WinnerDeclared",
+    type: "event",
   },
   {
-    "inputs": [],
-    "name": "admin",
-    "outputs": [
+    inputs: [],
+    name: "admin",
+    outputs: [
       {
-        "internalType": "address",
-        "name": "",
-        "type": "address"
-      }
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
     ],
-    "stateMutability": "view",
-    "type": "function",
-    "constant": true
+    stateMutability: "view",
+    type: "function",
+    constant: true,
   },
   {
-    "inputs": [
+    inputs: [
       {
-        "internalType": "uint256",
-        "name": "",
-        "type": "uint256"
-      }
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
     ],
-    "name": "events",
-    "outputs": [
+    name: "events",
+    outputs: [
       {
-        "internalType": "uint256",
-        "name": "eventId",
-        "type": "uint256"
+        internalType: "uint256",
+        name: "eventId",
+        type: "uint256",
       },
       {
-        "internalType": "string",
-        "name": "name",
-        "type": "string"
+        internalType: "string",
+        name: "name",
+        type: "string",
       },
       {
-        "internalType": "string",
-        "name": "description",
-        "type": "string"
+        internalType: "string",
+        name: "description",
+        type: "string",
       },
       {
-        "internalType": "string",
-        "name": "imageURL",
-        "type": "string"
+        internalType: "string",
+        name: "imageURL",
+        type: "string",
       },
       {
-        "internalType": "uint256",
-        "name": "startTime",
-        "type": "uint256"
+        internalType: "uint256",
+        name: "startTime",
+        type: "uint256",
       },
       {
-        "internalType": "uint256",
-        "name": "endTime",
-        "type": "uint256"
+        internalType: "uint256",
+        name: "endTime",
+        type: "uint256",
       },
       {
-        "internalType": "bool",
-        "name": "isCompleted",
-        "type": "bool"
+        internalType: "bool",
+        name: "isCompleted",
+        type: "bool",
       },
       {
-        "internalType": "string",
-        "name": "winningOption",
-        "type": "string"
+        internalType: "string",
+        name: "winningOption",
+        type: "string",
       },
       {
-        "internalType": "uint256",
-        "name": "prizePool",
-        "type": "uint256"
+        internalType: "uint256",
+        name: "prizePool",
+        type: "uint256",
       },
       {
-        "internalType": "string",
-        "name": "rules",
-        "type": "string"
+        internalType: "string",
+        name: "notificationMessage",
+        type: "string",
       },
-      {
-        "internalType": "string",
-        "name": "notificationImageURL",
-        "type": "string"
-      },
-      {
-        "internalType": "string",
-        "name": "notificationMessage",
-        "type": "string"
-      }
     ],
-    "stateMutability": "view",
-    "type": "function",
-    "constant": true
+    stateMutability: "view",
+    type: "function",
+    constant: true,
   },
   {
-    "inputs": [],
-    "name": "nextEventId",
-    "outputs": [
+    inputs: [],
+    name: "nextEventId",
+    outputs: [
       {
-        "internalType": "uint256",
-        "name": "",
-        "type": "uint256"
-      }
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
     ],
-    "stateMutability": "view",
-    "type": "function",
-    "constant": true
+    stateMutability: "view",
+    type: "function",
+    constant: true,
   },
   {
-    "inputs": [],
-    "name": "totalAdminProfit",
-    "outputs": [
+    inputs: [
       {
-        "internalType": "uint256",
-        "name": "",
-        "type": "uint256"
-      }
+        internalType: "uint256",
+        name: "_eventId",
+        type: "uint256",
+      },
+      {
+        internalType: "string",
+        name: "_name",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "_description",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "_imageURL",
+        type: "string",
+      },
+      {
+        internalType: "string[]",
+        name: "_options",
+        type: "string[]",
+      },
+      {
+        internalType: "uint256",
+        name: "_startTime",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "_endTime",
+        type: "uint256",
+      },
+      {
+        internalType: "string",
+        name: "_notificationMessage",
+        type: "string",
+      },
     ],
-    "stateMutability": "view",
-    "type": "function",
-    "constant": true
+    name: "createEvent",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
   },
   {
-    "inputs": [
+    inputs: [
       {
-        "internalType": "uint256",
-        "name": "_eventId",
-        "type": "uint256"
+        internalType: "uint256",
+        name: "_eventId",
+        type: "uint256",
       },
       {
-        "internalType": "string",
-        "name": "_name",
-        "type": "string"
+        internalType: "string",
+        name: "_option",
+        type: "string",
       },
-      {
-        "internalType": "string",
-        "name": "_description",
-        "type": "string"
-      },
-      {
-        "internalType": "string",
-        "name": "_imageURL",
-        "type": "string"
-      },
-      {
-        "internalType": "string[]",
-        "name": "_options",
-        "type": "string[]"
-      },
-      {
-        "internalType": "uint256",
-        "name": "_startTime",
-        "type": "uint256"
-      },
-      {
-        "internalType": "uint256",
-        "name": "_endTime",
-        "type": "uint256"
-      },
-      {
-        "internalType": "string",
-        "name": "_rules",
-        "type": "string"
-      },
-      {
-        "internalType": "string",
-        "name": "_notificationImageURL",
-        "type": "string"
-      },
-      {
-        "internalType": "string",
-        "name": "_notificationMessage",
-        "type": "string"
-      }
     ],
-    "name": "createEvent",
-    "outputs": [],
-    "stateMutability": "nonpayable",
-    "type": "function"
+    name: "placeBet",
+    outputs: [],
+    stateMutability: "payable",
+    type: "function",
+    payable: true,
   },
   {
-    "inputs": [
+    inputs: [
       {
-        "internalType": "uint256",
-        "name": "_eventId",
-        "type": "uint256"
+        internalType: "uint256",
+        name: "_eventId",
+        type: "uint256",
       },
       {
-        "internalType": "string",
-        "name": "_option",
-        "type": "string"
-      }
+        internalType: "string",
+        name: "_winningOption",
+        type: "string",
+      },
     ],
-    "name": "placeBet",
-    "outputs": [],
-    "stateMutability": "payable",
-    "type": "function",
-    "payable": true
+    name: "declareWinner",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
   },
   {
-    "inputs": [
+    inputs: [
       {
-        "internalType": "uint256",
-        "name": "_eventId",
-        "type": "uint256"
+        internalType: "uint256",
+        name: "_eventId",
+        type: "uint256",
+      },
+    ],
+    name: "getEvent",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "eventId",
+        type: "uint256",
       },
       {
-        "internalType": "string",
-        "name": "_winningOption",
-        "type": "string"
-      }
+        internalType: "string",
+        name: "name",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "description",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "imageURL",
+        type: "string",
+      },
+      {
+        internalType: "string[]",
+        name: "options",
+        type: "string[]",
+      },
+      {
+        internalType: "uint256",
+        name: "startTime",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "endTime",
+        type: "uint256",
+      },
+      {
+        internalType: "bool",
+        name: "isCompleted",
+        type: "bool",
+      },
+      {
+        internalType: "string",
+        name: "winningOption",
+        type: "string",
+      },
+      {
+        internalType: "uint256",
+        name: "prizePool",
+        type: "uint256",
+      },
+      {
+        internalType: "string",
+        name: "notificationMessage",
+        type: "string",
+      },
     ],
-    "name": "declareWinner",
-    "outputs": [],
-    "stateMutability": "nonpayable",
-    "type": "function"
+    stateMutability: "view",
+    type: "function",
+    constant: true,
   },
   {
-    "inputs": [
+    inputs: [
       {
-        "internalType": "uint256",
-        "name": "_eventId",
-        "type": "uint256"
-      }
+        internalType: "uint256",
+        name: "_eventId",
+        type: "uint256",
+      },
     ],
-    "name": "getEvent",
-    "outputs": [
+    name: "getEventOptions",
+    outputs: [
       {
-        "components": [
+        internalType: "string[]",
+        name: "",
+        type: "string[]",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+    constant: true,
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "_eventId",
+        type: "uint256",
+      },
+    ],
+    name: "getEventPrizePool",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+    constant: true,
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "_eventId",
+        type: "uint256",
+      },
+    ],
+    name: "getEventOdds",
+    outputs: [
+      {
+        components: [
           {
-            "internalType": "uint256",
-            "name": "eventId",
-            "type": "uint256"
+            internalType: "string",
+            name: "optionName",
+            type: "string",
           },
           {
-            "internalType": "string",
-            "name": "name",
-            "type": "string"
+            internalType: "uint256",
+            name: "oddsPercentage",
+            type: "uint256",
           },
-          {
-            "internalType": "string",
-            "name": "description",
-            "type": "string"
-          },
-          {
-            "internalType": "string",
-            "name": "imageURL",
-            "type": "string"
-          },
-          {
-            "internalType": "string[]",
-            "name": "options",
-            "type": "string[]"
-          },
-          {
-            "internalType": "uint256",
-            "name": "startTime",
-            "type": "uint256"
-          },
-          {
-            "internalType": "uint256",
-            "name": "endTime",
-            "type": "uint256"
-          },
-          {
-            "internalType": "bool",
-            "name": "isCompleted",
-            "type": "bool"
-          },
-          {
-            "internalType": "string",
-            "name": "winningOption",
-            "type": "string"
-          },
-          {
-            "internalType": "uint256",
-            "name": "prizePool",
-            "type": "uint256"
-          },
-          {
-            "internalType": "string",
-            "name": "rules",
-            "type": "string"
-          },
-          {
-            "internalType": "string",
-            "name": "notificationImageURL",
-            "type": "string"
-          },
-          {
-            "internalType": "string",
-            "name": "notificationMessage",
-            "type": "string"
-          }
         ],
-        "internalType": "struct BettingEvents.BetEventView",
-        "name": "",
-        "type": "tuple"
-      }
+        internalType: "struct BettingEvents.OptionOdds[]",
+        name: "",
+        type: "tuple[]",
+      },
     ],
-    "stateMutability": "view",
-    "type": "function",
-    "constant": true
+    stateMutability: "view",
+    type: "function",
+    constant: true,
   },
-  {
-    "inputs": [
-      {
-        "internalType": "uint256",
-        "name": "_eventId",
-        "type": "uint256"
-      }
-    ],
-    "name": "getEventOptions",
-    "outputs": [
-      {
-        "internalType": "string[]",
-        "name": "",
-        "type": "string[]"
-      }
-    ],
-    "stateMutability": "view",
-    "type": "function",
-    "constant": true
-  },
-  {
-    "inputs": [
-      {
-        "internalType": "uint256",
-        "name": "_eventId",
-        "type": "uint256"
-      }
-    ],
-    "name": "getEventPrizePool",
-    "outputs": [
-      {
-        "internalType": "uint256",
-        "name": "",
-        "type": "uint256"
-      }
-    ],
-    "stateMutability": "view",
-    "type": "function",
-    "constant": true
-  },
-  {
-    "inputs": [
-      {
-        "internalType": "uint256",
-        "name": "_eventId",
-        "type": "uint256"
-      }
-    ],
-    "name": "getEventOdds",
-    "outputs": [
-      {
-        "components": [
-          {
-            "internalType": "string",
-            "name": "optionName",
-            "type": "string"
-          },
-          {
-            "internalType": "uint256",
-            "name": "oddsPercentage",
-            "type": "uint256"
-          }
-        ],
-        "internalType": "struct BettingEvents.OptionOdds[]",
-        "name": "",
-        "type": "tuple[]"
-      }
-    ],
-    "stateMutability": "view",
-    "type": "function",
-    "constant": true
-  },
-  {
-    "inputs": [],
-    "name": "getTotalAdminProfit",
-    "outputs": [
-      {
-        "internalType": "uint256",
-        "name": "",
-        "type": "uint256"
-      }
-    ],
-    "stateMutability": "view",
-    "type": "function",
-    "constant": true
-  }
 ];
